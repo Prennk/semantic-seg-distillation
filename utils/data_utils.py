d4c4faf329825b6ec2ab4cfcba856f4ad5335a30
Prennk <email.affanardana@gmail.com>
2024-08-06 11:19:06 +0700

    fix import

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -4,7 +4,7 @@ import torchvision.transforms as transforms
 from utils.transforms import PILToLongTensor, LongTensorToRGBPIL
 from PIL import Image
 
-from utils import batch_transform, imshow_batch
+from utils.utils import batch_transform, imshow_batch
 from data.utils import enet_weighing, median_freq_balancing
 
 def load_dataset(dataset, args):
