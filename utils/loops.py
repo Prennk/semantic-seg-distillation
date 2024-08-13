878e7dcc21865c6cbc03c675d00d485203a3a4ea
Prennk <email.affanardana@gmail.com>
2024-08-13 09:52:47 +0700

    fix modulelist for vid

--- utils/loops.py
+++ utils/loops.py
@@ -210,10 +210,10 @@ class Distill:
             distill_loss = 0.0
 
             # Distill loss
-            for t_layer, s_layer in zip(self.t_model.layers_to_hook, self.s_model.layers_to_hook):
+            for idx, (t_layer, s_layer) in enumerate(zip(self.t_model.layers_to_hook, self.s_model.layers_to_hook)):
                 t_features = self.t_model.get_feature_map(t_layer)
                 s_features = self.s_model.get_feature_map(s_layer)
-                distill_loss += self.distill_criterion(s_features, t_features)
+                distill_loss += self.distill_criterion[idx](s_features, t_features)
 
             # Total loss
             total_loss = loss + distill_loss
