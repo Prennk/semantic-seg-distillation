7db0bab45572be6c92f49784331400a5b3e698f9
Prennk <email.affanardana@gmail.com>
2024-08-13 07:37:07 +0700

    fix model need register hook

--- main.py
+++ main.py
@@ -246,14 +246,14 @@ def distill(train_loader, val_loader, class_weights, class_encoding, args):
 
     # create teacher
     print(f"\nLoading teacher model: deeplabv3 from {args.teacher_path}...")
-    t_model = Create_DeepLabV3(num_classes, args).to(args.device)
+    t_model = Create_DeepLabV3(num_classes, args, layers_to_hook=args.teacher_layers).to(args.device)
 
     # load pretrained teacher
     teacher_dict = torch.load(args.teacher_path, map_location=args.device)["state_dict"]
     t_model.load_state_dict(teacher_dict)
 
     print(f"Creating student model: enet...")
-    s_model = Create_ENet(num_classes).to(args.device)
+    s_model = Create_ENet(num_classes, layers_to_hook=args.student_layers).to(args.device)
     
     # send model to wandb
     # wandb.watch(model, log="all")
