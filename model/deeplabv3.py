749c4b9193e34a3d550cfc8c87f6406e62a02a20
Prennk <email.affanardana@gmail.com>
2024-08-13 15:04:47 +0700

    fix code for distillation

--- model/deeplabv3.py
+++ model/deeplabv3.py
@@ -14,19 +14,25 @@ class Create_DeepLabV3(nn.Module):
     """
     def __init__(self, num_classes, args, layers_to_hook=None):
         super(Create_DeepLabV3, self).__init__()
-        if args.pretrained == "backbone":
-            print("Loading pretrained ResNet50 IMAGENET1K_V2...")
-            weights_backbone = models.ResNet50_Weights.IMAGENET1K_V2
-            weights = None
-        elif args.pretrained == "all":
-            print("Loading pretrained ResNet50 COCO_WITH_VOC_LABELS_V1...")
-            weights_backbone = None
-            weights = seg_model.DeepLabV3_ResNet50_Weights.COCO_WITH_VOC_LABELS_V1
-        elif not args.pretrained:
+        if args.mode in ["train", "test"]:
+            if args.pretrained == "backbone":
+                print("Loading pretrained ResNet50 IMAGENET1K_V2...")
+                weights_backbone = models.ResNet50_Weights.IMAGENET1K_V2
+                weights = None
+            elif args.pretrained == "all":
+                print("Loading pretrained ResNet50 COCO_WITH_VOC_LABELS_V1...")
+                weights_backbone = None
+                weights = seg_model.DeepLabV3_ResNet50_Weights.COCO_WITH_VOC_LABELS_V1
+            elif not args.pretrained:
+                weights_backbone = None
+                weights = None
+            else:
+                raise ValueError(f"Unknown pretrained command: {args.pretrained}")
+        elif args.mode == "distill":
             weights_backbone = None
             weights = None
         else:
-            raise ValueError(f"Unknown pretrained command: {args.pretrained}")
+            raise ValueError(f"Unknown argument {args.mode}")
 
         self.model = seg_model.deeplabv3_resnet50(
                 weights=weights, 
@@ -35,18 +41,23 @@ class Create_DeepLabV3(nn.Module):
         self.model.classifier[4] = nn.Conv2d(256, num_classes, kernel_size=1)
         self.model.aux_classifier[4] = nn.Conv2d(256, num_classes, kernel_size=1)
 
-        if args.pretrained and args.freeze and args.mode == ["train", "test"]:
-            # freeze deeplabv3 backbone
-            print(f"Freezing backbone...")
-            print(f"Trainable: DeepLabV3 head => ASPP + classifier")
-            self.model.aux_classifier = None
-            for param in self.model.backbone.parameters():
-                param.requires_grad = False
-        elif args.pretrained and not args.freeze:
-            print("[Warning] Pretrained layers is trainable")
-        elif not args.pretrained and args.freeze:
-            raise ValueError("You freeze the untrain model backbone")
-            
+        if args.mode in ["train", "test"]:
+            if args.pretrained and args.freeze:
+                # freeze deeplabv3 backbone
+                print(f"Freezing backbone...")
+                print(f"Trainable: DeepLabV3 head => ASPP + classifier")
+                self.model.aux_classifier = None
+                for param in self.model.backbone.parameters():
+                    param.requires_grad = False
+            elif args.pretrained and not args.freeze:
+                print("[Warning] Pretrained layers is trainable")
+            elif not args.pretrained and args.freeze:
+                raise ValueError("You freeze the untrain model backbone")
+        elif args.mode == "distill":
+            print("Preparing DeepLabV3 for teacher...")
+        else:
+            raise ValueError(f"Unknown argument {args.mode}")
+                
         self.feature_maps = {}
         self.layers_to_hook = layers_to_hook or []
         self._register_hooks() 
