d67e7c1059d3bea523ede271a734f949e4dfcb66
Prennk <email.affanardana@gmail.com>
2024-08-13 14:26:21 +0700

    update col width

--- summary.py
+++ summary.py
@@ -19,7 +19,7 @@ def show_summary(model):
     summary(model=model,
             input_size=(1, 3, args.width, args.height),
             col_names=["input_size", "output_size", "num_params", "params_percent", "kernel_size", "mult_adds", "trainable"],
-            col_width=40,
+            col_width=20,
             device=args.device,
             row_settings=["var_names"],
             verbose=2)
