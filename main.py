8d95b28104f26850b7ba40ea4dd14b6558b1db2e
Prennk <email.affanardana@gmail.com>
2024-08-14 10:01:33 +0700

    debug predict

--- main.py
+++ main.py
@@ -330,7 +330,7 @@ def distill(train_loader, val_loader, class_weights, class_encoding, args):
             best_epoch = epoch
             utils.save_checkpoint(s_model, optimizer, epoch + 1, best_miou, best_mpa, args)
 
-        if (epoch + 1) % 10 == 0 or (epoch + 1) == args.epochs:
+        if (epoch + 1) % 1 == 0 or (epoch + 1) == args.epochs:
             # predict the segmentation map and send it to wandb
             images, _ = next(iter(val_loader))
             predict(s_model, images[:1], class_encoding, epoch)
