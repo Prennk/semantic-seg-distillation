8e370f1fcb9d2d059f289093793370662ef91751
Prennk <email.affanardana@gmail.com>
2024-08-19 10:08:55 +0700

    fix summary print twice

--- main.py
+++ main.py
@@ -60,11 +60,10 @@ def train(train_loader, val_loader, class_weights, class_encoding, args):
         raise TypeError('Invalid model name. Available models are enet and deeplabv3')
 
     # print model summary
-    model_summary = summary(model=model,
-                            input_data=torch.randn(1, 3, args.width, args.height).to(args.device),
-                            col_names=["trainable"],
-                            row_settings=["var_names"])
-    print(model_summary)
+    summary(model=model,
+            input_data=torch.randn(1, 3, args.width, args.height).to(args.device),
+            col_names=["trainable"],
+            row_settings=["var_names"])
 
     criterion = nn.CrossEntropyLoss(weight=class_weights)
     optimizer = optim.SGD(
