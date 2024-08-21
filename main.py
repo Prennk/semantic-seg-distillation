8f5bab84a51fd89d3693d2fe6dc02461c423ad32
Prennk <email.affanardana@gmail.com>
2024-08-21 08:07:19 +0700

    fix summary

--- main.py
+++ main.py
@@ -61,10 +61,10 @@ def train(train_loader, val_loader, class_weights, class_encoding, args):
         raise TypeError('Invalid model name. Available models are enet and deeplabv3_resnet101')
 
     # print model summary
-    summary(model=model,
+    print(summary(model=model,
             input_data=torch.randn(1, 3, args.width, args.height).to(args.device),
             col_names=["trainable"],
-            row_settings=["var_names"])
+            row_settings=["var_names"]))
 
     criterion = nn.CrossEntropyLoss(weight=class_weights)
     optimizer = optim.SGD(
