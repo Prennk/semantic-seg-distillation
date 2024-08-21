a0f51131df0593af20d116aba33671c184a88dd6
Prennk <email.affanardana@gmail.com>
2024-08-21 07:43:36 +0700

    fix isinstance

--- utils/loops.py
+++ utils/loops.py
@@ -50,10 +50,8 @@ class Train:
 
             # Forward propagation
             outputs = self.model(inputs)
-            print(type(outputs))
-            print(outputs)
 
-            if isinstance(outputs, OrderedDict or tuple):
+            if isinstance(outputs, (OrderedDict, tuple)):
                 aux_outputs = outputs['aux']
                 classifier_outputs = outputs['out']
 
@@ -133,7 +131,7 @@ class Test:
                 # Forward propagation
                 outputs = self.model(inputs)
                 
-                if isinstance(outputs, OrderedDict or tuple):
+                if isinstance(outputs, (OrderedDict, tuple)):
                     outputs = outputs['out']
 
                 # Loss computation
@@ -203,12 +201,12 @@ class Distill:
             # Forward propagation for teacher
             with torch.inference_mode():
                 t_outputs = self.t_model(inputs)
-                if isinstance(t_outputs, OrderedDict or tuple):
+                if isinstance(t_outputs, (OrderedDict, tuple)):
                     t_outputs = t_outputs['out']
 
             # Forward propagation for student
             s_outputs = self.s_model(inputs)
-            if isinstance(s_outputs, OrderedDict or tuple):
+            if isinstance(s_outputs, (OrderedDict, tuple)):
                 s_outputs = s_outputs['out']
 
             # Loss computation
