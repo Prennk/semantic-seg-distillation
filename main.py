a6a095f0dcb89639c5badd02819154717d75b295
Prennk <email.affanardana@gmail.com>
2024-08-25 13:43:37 +0700

    fix ignore index in metrics

--- main.py
+++ main.py
@@ -78,12 +78,12 @@ def train(train_loader, val_loader, class_weights, class_encoding, args):
     lr_updater = optim.lr_scheduler.LambdaLR(optimizer, lambda_lr)
 
     # Evaluation metric
-    # if args.ignore_unlabeled:
-    #     ignore_index = list(class_encoding).index('unlabeled')
-    # else:
-    ignore_index = None
-    metric_iou = metrics.IoU(num_classes, ignore_index=ignore_index)
-    metric_pa = metrics.PixelAccuracy(num_classes, ignore_index=ignore_index)
+    if args.ignore_unlabeled:
+        ignore_index = list(class_encoding).index('unlabeled')
+    else:
+        ignore_index = None
+    metric_iou = metrics.IoU(num_classes)
+    metric_pa = metrics.PixelAccuracy(num_classes)
 
     # Optionally resume from a checkpoint
     if args.resume:
