c3ce1d920516fd38fa99113736ca53e1ffb5ee58
Prennk <email.affanardana@gmail.com>
2024-08-25 14:11:06 +0700

    fix numpy

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -3,6 +3,7 @@ import torch.utils.data as data
 import torchvision.transforms as transforms
 from utils.transforms import PILToLongTensor, LongTensorToRGBPIL
 from PIL import Image
+import numpy as np
 
 from utils.utils import batch_transform, imshow_batch
 from data.utils import enet_weighing, median_freq_balancing
