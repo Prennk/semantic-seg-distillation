fix hook doesn't  backpropagate

distiller/vid.py
@@ -23,42 +23,14 @@ class VIDLoss(nn.Module):
                 kernel_size=1, padding=0,
                 bias=False, stride=stride)
 
-        # self.regressor = nn.Sequential(
-        #     conv1x1(num_input_channels, num_mid_channel),
-        #     nn.ReLU(),
-        #     conv1x1(num_mid_channel, num_mid_channel),
-        #     nn.ReLU(),
-        #     conv1x1(num_mid_channel, num_target_channels),
-        # )
-
         self.regressor = nn.Sequential(
             conv1x1(num_input_channels, num_mid_channel),
             nn.ReLU(),
             conv1x1(num_mid_channel, num_mid_channel),
             nn.ReLU(),
-
-            conv1x1(num_mid_channel, num_mid_channel),
-            nn.ReLU(),
-            conv1x1(num_mid_channel, num_mid_channel),
-            nn.ReLU(),
-
-            conv1x1(num_mid_channel, num_mid_channel),
-            nn.ReLU(),
-            conv1x1(num_mid_channel, num_mid_channel),
-            nn.ReLU(),
-
-            conv1x1(num_mid_channel, num_mid_channel),
-            nn.ReLU(),
-            conv1x1(num_mid_channel, num_mid_channel),
-            nn.ReLU(),
-
-            conv1x1(num_mid_channel, num_mid_channel),
-            nn.ReLU(),
-            conv1x1(num_mid_channel, num_mid_channel),
-            nn.ReLU(),
-
             conv1x1(num_mid_channel, num_target_channels),
         )
+
         self.log_scale = torch.nn.Parameter(
             np.log(np.exp(init_pred_var-eps)-1.0) * torch.ones(num_target_channels)
             )

model/deeplabv3_torchvision.py
@@ -47,14 +47,15 @@ class Create_DeepLabV3(nn.Module):
 
     def forward(self, x):
         output = self.model(x)
+        intermediate_features = {name: self.feature_maps.get(name, None) for name in self.layers_to_hook}
 
-        return output
+        return output, intermediate_features
 
     def _register_hooks(self):
         available_layers = [name for name, _ in self.model.named_modules()]
         invalid_layers = [layer for layer in self.layers_to_hook if layer not in available_layers]
         if invalid_layers:
-            raise NameError("The following layers are not found in the model: {invalid_layers}")
+            raise NameError(f"The following layers are not found in the model: {invalid_layers}")
 
         for name, module in self.model.named_modules():
             if name in self.layers_to_hook:
@@ -62,9 +63,6 @@ class Create_DeepLabV3(nn.Module):
     
     def _hook(self, layer_name):
         def hook_fn(module, input, output):
-            self.feature_maps[layer_name] = output.detach()
+            self.feature_maps[layer_name] = output
 
         return hook_fn
-
-    def get_feature_map(self, layer_name):
-        return self.feature_maps.get(layer_name, None)

utils/loops.py
@@ -205,12 +205,12 @@ class Distill:
 
             # Forward propagation for teacher
             with torch.inference_mode():
-                t_outputs = self.t_model(inputs)
+                t_outputs, t_intermediate_features = self.t_model(inputs)
                 if isinstance(t_outputs, OrderedDict):
                     t_outputs = t_outputs['out']
 
             # Forward propagation for student
-            s_outputs = self.s_model(inputs)
+            s_outputs, s_intermediate_features = self.s_model(inputs)
             if isinstance(s_outputs, OrderedDict):
                 s_outputs = s_outputs['out']
 
@@ -222,11 +222,11 @@ class Distill:
 
             # Distill loss
             self.distill_criterion.to(self.device)
-            for idx, (t_layer, s_layer) in enumerate(zip(self.t_model.layers_to_hook, self.s_model.layers_to_hook)):
-                t_features = self.t_model.get_feature_map(t_layer)
-                s_features = self.s_model.get_feature_map(s_layer)
+            for idx, layer_name in enumerate(self.t_model.layers_to_hook):
+                t_features = t_intermediate_features[layer_name]
+                s_features = s_intermediate_features[layer_name]
                 distill_loss += self.distill_criterion[idx](s_features, t_features)
-
+                
             # Total loss
             total_loss = loss + distill_loss
 