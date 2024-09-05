6e839e70c72d6c7a3ccdaea5402522cf1d84fd7b
Prennk <email.affanardana@gmail.com>
2024-09-05 16:26:56 +0700

    add dlv resnet18 cirkd

--- main.py
+++ main.py
@@ -60,8 +60,10 @@ def train(train_loader, val_loader, class_weights, class_encoding, args):
         model = Create_ENet(num_classes).to(args.device)
     elif args.model == "deeplabv3_torch":
         model = Create_DeepLabV3(num_classes, args).to(args.device)
-    elif args.model == "deeplabv3_cirkd":
+    elif args.model == "deeplabv3_cirkd_resnet101":
         model = get_deeplabv3(num_classes=num_classes, backbone="resnet101", pretrained=True, args=args).to(args.device)
+    elif args.model == "deeplabv3_cirkd_resnet18":
+        model = get_deeplabv3(num_classes=num_classes, backbone="resnet18", pretrained=True, args=args).to(args.device)
     else:
         raise TypeError('Invalid model name. Available models are enet and deeplabv3_resnet101')
 
