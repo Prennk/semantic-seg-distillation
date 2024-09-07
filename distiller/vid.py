275705dec3030d77c58a3d4a28c04a53fc635f29
Prennk <email.affanardana@gmail.com>
2024-09-07 18:21:13 +0700

    :v

--- distiller/vid.py
+++ distiller/vid.py
@@ -1,18 +1,75 @@
-from __future__ import print_function
+# from __future__ import print_function
+
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# import numpy as np
+
+
+# class VIDLoss(nn.Module):
+#     """Variational Information Distillation for Knowledge Transfer (CVPR 2019),
+#     code from author: https://github.com/ssahn0215/variational-information-distillation"""
+#     def __init__(self,
+#                  num_input_channels,
+#                  num_mid_channel,
+#                  num_target_channels,
+#                  init_pred_var=5.0,
+#                  eps=1e-5):
+#         super(VIDLoss, self).__init__()
+
+#         def conv1x1(in_channels, out_channels, stride=1):
+#             return nn.Conv2d(
+#                 in_channels, out_channels,
+#                 kernel_size=1, padding=0,
+#                 bias=False, stride=stride)
+
+#         self.regressor = nn.Sequential(
+#             conv1x1(num_input_channels, num_mid_channel),
+#             nn.ReLU(),
+#             conv1x1(num_mid_channel, num_mid_channel),
+#             nn.ReLU(),
+#             conv1x1(num_mid_channel, num_target_channels),
+#         )
+
+#         self.log_scale = torch.nn.Parameter(
+#             np.log(np.exp(init_pred_var-eps)-1.0) * torch.ones(num_target_channels)
+#             )
+#         self.eps = eps
+
+#     def forward(self, input, target):
+#         # pool for dimension match
+#         s_H, s_W = input.shape[2], input.shape[3]
+#         t_H, t_W = target.shape[2], target.shape[3]
+
+#         if s_H > t_H:
+#             input = F.adaptive_avg_pool2d(input, (t_H, t_W))
+#         elif s_H < t_H:
+#             target = F.adaptive_avg_pool2d(target, (s_H, s_W))
+#         else:
+#             pass
+
+#         pred_mean = self.regressor(input)
+#         pred_var = torch.log(1.0+torch.exp(self.log_scale))+self.eps
+#         pred_var = pred_var.view(1, -1, 1, 1)
+#         neg_log_prob = 0.5*(
+#             (pred_mean-target)**2/pred_var+torch.log(pred_var)
+#             )
+#         loss = torch.mean(neg_log_prob)
+#         return loss
+
+# =========================
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-
 class VIDLoss(nn.Module):
-    """Variational Information Distillation for Knowledge Transfer (CVPR 2019),
-    code from author: https://github.com/ssahn0215/variational-information-distillation"""
     def __init__(self,
                  num_input_channels,
                  num_mid_channel,
                  num_target_channels,
+                 num_segments=12,
                  init_pred_var=5.0,
                  eps=1e-5):
         super(VIDLoss, self).__init__()
@@ -23,55 +80,51 @@ class VIDLoss(nn.Module):
                 kernel_size=1, padding=0,
                 bias=False, stride=stride)
 
-        # self.regressor = nn.Sequential(
-        #     conv1x1(num_input_channels, num_mid_channel),
-        #     nn.ReLU(),
-        #     conv1x1(num_mid_channel, num_mid_channel),
-        #     nn.ReLU(),
-        #     conv1x1(num_mid_channel, num_target_channels),
-        # )
-
-        self.regressor = nn.Sequential(
-            conv1x1(num_input_channels, num_input_channels * 2),
-            nn.ReLU(),
-            conv1x1(num_input_channels * 2, num_input_channels * 4),
-            nn.ReLU(),
-            conv1x1(num_input_channels * 4, num_input_channels * 8),
-            nn.ReLU(),
-            conv1x1(num_input_channels * 8, num_mid_channel // 2),
-            nn.ReLU(),
-            conv1x1(num_mid_channel // 2, num_mid_channel),
-            nn.ReLU(),
-            conv1x1(num_mid_channel, num_mid_channel),
-            nn.ReLU(),
-            conv1x1(num_mid_channel, num_target_channels),
-            nn.ReLU(),
-            conv1x1(num_target_channels, num_target_channels),
-        )
-
-
-        self.log_scale = torch.nn.Parameter(
-            np.log(np.exp(init_pred_var-eps)-1.0) * torch.ones(num_target_channels)
+        self.regressors = nn.ModuleList([
+            nn.Sequential(
+                conv1x1(num_input_channels, num_mid_channel),
+                nn.ReLU(),
+                conv1x1(num_mid_channel, num_mid_channel),
+                nn.ReLU(),
+                conv1x1(num_mid_channel, num_target_channels),
             )
+            for _ in range(num_segments)
+        ])
+
+        self.log_scales = nn.ParameterList([
+            torch.nn.Parameter(
+                np.log(np.exp(init_pred_var - eps) - 1.0) * torch.ones(num_target_channels)
+            ) for _ in range(num_segments)
+        ])
+
         self.eps = eps
+        self.num_segments = num_segments
 
-    def forward(self, input, target):
-        # pool for dimension match
-        s_H, s_W = input.shape[2], input.shape[3]
-        t_H, t_W = target.shape[2], target.shape[3]
+    def forward(self, inputs, targets):
+        # Assumes inputs and targets are lists of tensors for each segment
+        assert len(inputs) == self.num_segments, "Mismatch in number of input segments"
+        assert len(targets) == self.num_segments, "Mismatch in number of target segments"
 
-        if s_H > t_H:
-            input = F.adaptive_avg_pool2d(input, (t_H, t_W))
-        elif s_H < t_H:
-            target = F.adaptive_avg_pool2d(target, (s_H, s_W))
-        else:
-            pass
+        total_loss = 0
+        for i in range(self.num_segments):
+            input, target = inputs[i], targets[i]
 
-        pred_mean = self.regressor(input)
-        pred_var = torch.log(1.0+torch.exp(self.log_scale))+self.eps
-        pred_var = pred_var.view(1, -1, 1, 1)
-        neg_log_prob = 0.5*(
-            (pred_mean-target)**2/pred_var+torch.log(pred_var)
+            # Pool for dimension match
+            s_H, t_H = input.shape[2], target.shape[2]
+            if s_H > t_H:
+                input = F.adaptive_avg_pool2d(input, (t_H, t_H))
+            elif s_H < t_H:
+                target = F.adaptive_avg_pool2d(target, (s_H, s_H))
+
+            pred_mean = self.regressors[i](input)
+            pred_var = torch.log(1.0 + torch.exp(self.log_scales[i])) + self.eps
+            pred_var = pred_var.view(1, -1, 1, 1)
+
+            neg_log_prob = 0.5 * (
+                (pred_mean - target) ** 2 / pred_var + torch.log(pred_var)
             )
-        loss = torch.mean(neg_log_prob)
-        return loss
+
+            total_loss += torch.mean(neg_log_prob)
+
+        # Return the total loss
+        return total_loss / self.num_segments
