99005899f755b9bb97edea9eae389f336db94a0d
Prennk <email.affanardana@gmail.com>
2024-09-10 18:27:15 +0700

    fix get channels

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ def distill(train_loader, val_loader, class_weights, class_encoding, args):
     print(f"Student layer shapes: {s_dim}")
 
     criterion_kd = nn.ModuleList(
-        [VIDLoss(s, t, t) for s, t in zip(s_dim.shape[1], t_dim.shape[1])]   
+        [VIDLoss(s.shape[1], t.shape[1], t.shape[1]) for s, t in zip(s_dim, t_dim)]   
         )
 
     trainable_list.append(criterion_kd)
