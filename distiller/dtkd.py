00ca180d477fbe287f0c5fae81614af76647389f
Prennk <email.affanardana@gmail.com>
2024-09-25 09:57:33 +0700

    debug

--- distiller/dtkd.py
+++ distiller/dtkd.py
@@ -27,7 +27,7 @@ class DTKD(nn.Module):
             F.softmax(logits_teacher / logits_teacher_temp, dim=1)
         )
         loss_ourskd = (ourskd.sum(1, keepdim=True) * logits_teacher_temp * logits_student_temp).mean()
-        print(f"logits_student: {logits_student_max}")
+        print(f"logits_student: {logits_student}")
         print()
         print(f"logits_student_max: {logits_student_max}")
         print()
